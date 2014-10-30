ef3b3a7545bf6c18a0b707a57afc93d8dc660b3e
zeekvfu <wzmindlog@gmail.com>
2014-10-30 23:34:13 +0800

    multi_thread_continous_download.py, refactor code

--- multi_thread_continous_download.py
+++ multi_thread_continous_download.py
@@ -3,24 +3,24 @@
 # multi_thread_continous_download.py
 
 
-import os
-import threading
-import urllib.request
-import urllib.error
+import os, threading
+import urllib.request, urllib.error
 import shutil
 
-from socket import timeout
+import socket
+# from socket import timeout
 
 import utility
-from utility import get_file_size, split_file_size, split_file_name, append_file
+from utility import get_current_timestamp, get_file_size, split_file_size, get_file_name_split, append_file
 
 import single_thread_continous_download
 from single_thread_continous_download import single_thread_continous_download
 
 
+
+
 # 各个子线程下载自己负责的那部分内容
 def sub_thread_continous_download(url, file_name, begin, end, timeout_retry=True):
-	need_timeout_retry = False		# 仅从下载需求本身来看，是否需要超时重传。默认不需要
 	while True:
 		_begin = begin
 		if os.path.exists(file_name):
@@ -29,31 +29,32 @@ def sub_thread_continous_download(url, file_name, begin, end, timeout_retry=True
 				print("sub_thread_continous_download(): file size error!")
 				return
 			current_size = os.path.getsize(file_name)
-			# 此处认为已存在的同名文件，就是要下载的目标文件，只是未下载完而已，可以继续下载
+			# 此处认为已存在的同名文件，就是要下载的目标文件；只是未下载完而已，可以继续下载
 			if (current_size < target_size):
 				_begin += current_size
-			# 文件已下载完成
+			# 理论上来说，更严谨的方法是下载完目标文件，然后比较两个文件的 MD5 值。但是需要事先下载整个文件，可能浪费带宽（尤其是文件很大的时侯）
 			elif (current_size == target_size):
 				print("sub_thread_continous_download(): file %s already downloaded complete!" %(file_name))
 				return 
 			# 已存在的同名文件大小 > 要下载的目标文件大小，重命名已存在文件，重新下载目标文件
 			else:
-				print("sub_thread_continous_download(): file size exception, current file size bigger than target file size!")
-				new_file_name = file_name + '.backUP'
+				print("sub_thread_continous_download(): file size exception, current_size > target_size!")
+				new_file_name = file_name + '_' + get_current_timestamp()
 				os.rename(file_name, new_file_name)
-				print("sub_thread_continous_download(): %s renamed to %s ..." %(file_name, new_file_name))
+				print("sub_thread_continous_download(): %s RENAMED TO %s" %(file_name, new_file_name))
 		req = urllib.request.Request(url)
 		req.add_header('Range', 'bytes=%d-%d' % (_begin, end))
 		try:
 			with urllib.request.urlopen(req, timeout=300) as response, open(file_name, 'ab+') as out_stream:
 				shutil.copyfileobj(response, out_stream)
+			return 
 		except urllib.error.URLError as e:
-			print(e.errno, '\n', e.reason, '\n')
-		except timeout:
+			print("sub_thread_continous_download(): urllib.error.URLError\t", url, "\t", e.errno, "\t", e.reason)
+			return 
+		except socket.timeout:
 			print('sub_thread_continous_download(): socket time out ...')
-			need_timeout_retry = True
-		if (not timeout_retry or not need_timeout_retry):
-			break
+			if not timeout_retry:
+				break
 	return 
 
 
@@ -69,32 +70,38 @@ def multi_thread_continous_download(url, file_name=None, overwrite=False, thread
 		if (target_size < 0):
 			print("multi_thread_continous_download(): get_file_size() error!\n")
 			return
-		if os.path.exists(file_name) and (not overwrite):
+		if os.path.exists(file_name):
+			if overwrite:
+				os.remove(file_name)
 			current_size = os.path.getsize(file_name)
+			# 理论上来说，更严谨的方法是下载完目标文件，然后比较两个文件的 MD5 值。但是需要事先下载整个文件，可能浪费带宽（尤其是文件很大的时侯）
 			if (current_size == target_size):
 				print("multi_thread_continous_download(): file %s already downloaded complete!" %(file_name))
 				return 
 			# 已存在的同名文件大小 != 要下载的目标文件大小，重命名已存在文件，重新下载目标文件
 			else:
-				print("multi_thread_continous_download(): file size exception, current file size and target file size NOT same!")
-				new_file_name = file_name + '.backUP'
+				print("multi_thread_continous_download(): file size exception, current_size != target_size")
+				new_file_name = file_name + '_' + get_current_timestamp()
 				os.rename(file_name, new_file_name)
-				print("multi_thread_continous_download(): %s renamed to %s ..." %(file_name, new_file_name))
+				print("multi_thread_continous_download(): %s RENAMED TO %s" %(file_name, new_file_name))
 		ranges = split_file_size(target_size, thread_num)
 		thread_group = []
 		for i in range(thread_num):
 			# print(i, '\t', ranges[i][0], ',', ranges[i][1])
-			t = threading.Thread(target=sub_thread_continous_download, name="thread%d" % i, args=(url, split_file_name(file_name, i), ranges[i][0], ranges[i][1]))
+			t = threading.Thread(target=sub_thread_continous_download, name="thread%d" % i, args=(url, get_file_name_split(file_name, i), ranges[i][0], ranges[i][1]))
 			t.start()
 			thread_group.append(t)
 		for t in thread_group:
 			t.join()
-		append_file(file_name, thread_num, True)
-
+		append_file(file_name, thread_num)
 
 # multi_thread_continous_download("http://iweb.dl.sourceforge.net/project/zsh/zsh-doc/5.0.5/zsh-5.0.5-doc.tar.bz2", overwrite=True, thread_num=4)
 # multi_thread_continous_download("https://github.com/zeekvfu/sip_tea/archive/master.zip", overwrite=True, thread_num=4)
 # multi_thread_continous_download("http://screencasts.b0.upaiyun.com/podcasts/nil_podcast_1.m4a", overwrite=False, thread_num=4)
+
+
+# url = str(input('请输入要下载的目标文件的 URL：'))
+# multi_thread_continous_download(url, thread_num=4)
 
 
 
